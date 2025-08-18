02c3939bd0b21fd23a90ece3ab10ff606bed6a19
rayray830 <shirleyray830@gmail.com>
2025-08-19 02:23:04 +1000

    Jinglin

---
+++ app/build.gradle.kts
@@ -0,0 +1,51 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.google.services) // Jinglin 8.18.2025
+}
+
+android {
+    namespace = "unimelb.comp90018.equaltrip"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "unimelb.comp90018.equaltrip"
+        minSdk = 33
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    implementation("com.google.android.material:material:1.12.0")
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
+
+android { buildFeatures { viewBinding = true } }
---
+++ app/src/main/java/unimelb/comp90018/equaltrip/HomeActivity.java
@@ -0,0 +1,4 @@
+package unimelb.comp90018.equaltrip;
+
+public class HomeActivity {
+}
